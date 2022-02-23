ef3a604cd3b444af9c2aa590398291ee3020afd2
nedrech <denischernyy@ya.ru>
2022-02-23 21:21:37 +0300

    Added PersonsAdapter

---
+++ app/src/main/java/ru/nedrech/android/personslist/PersonsAdapter.kt
@@ -0,0 +1,24 @@
+package ru.nedrech.android.personslist
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.nedrech.android.personslist.databinding.AdapterPersonsBinding
+
+class PersonsAdapter : RecyclerView.Adapter<MainViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
+
+class MainViewHolder(binding: AdapterPersonsBinding) : RecyclerView.ViewHolder(binding.root) {
+
+}
