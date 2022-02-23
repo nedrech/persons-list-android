96b6b93b76d776b95fdc954c172014ba939a180e
nedrech <denischernyy@ya.ru>
2022-02-23 21:22:10 +0300

    Added Person entity

---
+++ app/src/main/java/ru/nedrech/android/personslist/Person.kt
@@ -0,0 +1,16 @@
+package ru.nedrech.android.personslist
+
+data class Person(
+
+    val firstName: String,
+
+    val secondName: String,
+
+    val midName: String?,
+
+    val role: String,
+
+    val photoUrl: String?,
+
+    val Description: String
+)
