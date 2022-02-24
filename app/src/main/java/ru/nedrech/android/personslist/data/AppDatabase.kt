c7c6712cf67f22d03857da6e9c9499c9b1fd2308
nedrech <denischernyy@ya.ru>
2022-02-24 14:07:54 +0300

    Add app database context

---
+++ app/src/main/java/ru/nedrech/android/personslist/data/AppDatabase.kt
@@ -0,0 +1,11 @@
+package ru.nedrech.android.personslist.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import ru.nedrech.android.personslist.data.dao.PeronDao
+import ru.nedrech.android.personslist.data.models.Person
+
+@Database(entities = [Person::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun personDao(): PeronDao
+}
