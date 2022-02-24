a84712858205c4790be0f060a0bf4520997a66be
nedrech <denischernyy@ya.ru>
2022-02-24 14:07:36 +0300

    Updated SELECT query

--- app/src/main/java/ru/nedrech/android/personslist/data/dao/PeronDao.kt
+++ app/src/main/java/ru/nedrech/android/personslist/data/dao/PeronDao.kt
@@ -12,7 +12,7 @@ interface PeronDao {
     @Query("SELECT * FROM persons")
     fun getAll(): List<Person>
 
-    @Query("SELECT * FROM persons WHERE id = :id")
+    @Query("SELECT * FROM persons WHERE id = :id LIMIT 1")
     fun getById(id: Int): Person
 
     @Insert
