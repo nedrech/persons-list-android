7072d44568175c83e702eece8e6f17f8de9d9e21
nedrech <denischernyy@ya.ru>
2022-02-24 14:31:47 +0300

    Added view data query

--- app/src/main/java/ru/nedrech/android/personslist/data/dao/PeronDao.kt
+++ app/src/main/java/ru/nedrech/android/personslist/data/dao/PeronDao.kt
@@ -1,5 +1,6 @@
 package ru.nedrech.android.personslist.data.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 import ru.nedrech.android.personslist.data.models.Person
 
@@ -8,6 +9,9 @@ interface PeronDao {
 
     @Query("SELECT * FROM persons")
     fun getAll(): List<Person>
+
+    @Query("SELECT first_name, second_name, middle_name, role, photo_url FROM persons")
+    fun getAllViewData(): LiveData<List<Person>>
 
     @Query("SELECT * FROM persons WHERE id = :id LIMIT 1")
     fun getById(id: Int): Person
