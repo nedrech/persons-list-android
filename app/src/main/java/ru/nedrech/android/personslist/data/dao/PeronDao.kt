e6943bd465c510e0daab052cb588cec9b7fa0ae3
nedrech <denischernyy@ya.ru>
2022-02-24 14:52:50 +0300

    Removed, fixed query

--- app/src/main/java/ru/nedrech/android/personslist/data/dao/PeronDao.kt
+++ app/src/main/java/ru/nedrech/android/personslist/data/dao/PeronDao.kt
@@ -1,6 +1,5 @@
 package ru.nedrech.android.personslist.data.dao
 
-import androidx.lifecycle.LiveData
 import androidx.room.*
 import ru.nedrech.android.personslist.data.models.Person
 
@@ -10,14 +9,11 @@ interface PeronDao {
     @Query("SELECT * FROM persons")
     fun getAll(): List<Person>
 
-    @Query("SELECT first_name, second_name, middle_name, role, photo_url FROM persons")
-    fun getAllViewData(): LiveData<List<Person>>
-
     @Query("SELECT * FROM persons WHERE id = :id LIMIT 1")
     fun getById(id: Int): Person
 
     @Insert
-    fun insertAll(vararg persons: Person)
+    fun insertAll(persons: List<Person>)
 
     @Delete
     fun delete(person: Person)
