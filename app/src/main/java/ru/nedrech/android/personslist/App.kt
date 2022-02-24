96bbd06f880ccc6f99608fe477d72753640ff249
nedrech <denischernyy@ya.ru>
2022-02-24 13:50:29 +0300

    Add Application class implementation

---
+++ app/src/main/java/ru/nedrech/android/personslist/App.kt
@@ -0,0 +1,16 @@
+package ru.nedrech.android.personslist
+
+import android.app.Application
+
+class App : Application() {
+
+    companion object {
+        lateinit var instance: App
+            private set
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
