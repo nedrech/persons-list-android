3ac891bbf3cf5267e611812f9fbd7ce9926bca43
nedrech <denischernyy@ya.ru>
2022-02-24 14:20:52 +0300

    Added database

--- app/src/main/java/ru/nedrech/android/personslist/App.kt
+++ app/src/main/java/ru/nedrech/android/personslist/App.kt
@@ -1,16 +1,20 @@
 package ru.nedrech.android.personslist
 
 import android.app.Application
+import androidx.room.Room
+import ru.nedrech.android.personslist.data.AppDatabase
 
 class App : Application() {
 
     companion object {
-        lateinit var instance: App
-            private set
+        lateinit var database: AppDatabase
     }
 
     override fun onCreate() {
         super.onCreate()
-        instance = this
+        database = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "app-database"
+        ).build()
     }
 }
